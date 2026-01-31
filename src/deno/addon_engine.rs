7630a9d73fcdc1e7e498deac249f28d815059841
Alex Woodman <alexthegoodman@gmail.com>
2026-01-31 02:07:57 -0500

    fix

--- src/deno/addon_engine.rs
+++ src/deno/addon_engine.rs
@@ -478,7 +478,7 @@ impl AddonEngine {
                         );
 
                         let landscape = Landscape::new(
-                            &format!("{}_landscape", addon_name),
+                            &Uuid::new_v4().to_string(),
                             &data,
                             &gpu.device,
                             &gpu.queue,
